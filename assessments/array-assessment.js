ff73c7c36995bca606b01fc2452f1a9d9081edec
Matthew Billie <matthewbillie@matthew.local>
2022-07-19 11:19:13 -0400

    reorganize + add array assessment

---
+++ assessments/array-assessment.js
@@ -0,0 +1,38 @@
+/*
+Array questions
+
+Cheat Sheet:
+https://dev.to/devsmitra/28-javascript-array-hacks-a-cheat-sheet-for-developer-5769
+*/
+
+const arrayOfStrings = ['hi', 'as', 'mean', 'gay', 'trannies', 'hitler', 'yes hello'];
+const arrayOfNumbers = [5, 4, 3, 4, 10, 9, 20, 420];
+
+// 1.
+// Iterate over the arrayOfStrings and console.log each item
+arrayOfStrings.forEach(item => console.log(item));
+
+// 2.
+// For arrayOfNumbers, return a new array where the values are multiplied by 10
+// expected output: [50, 40, 30...]
+
+
+// 3.
+// Find all of the values in arrayOfNumbers that are > 5
+
+
+// 4.
+// Sort arrayOfStrings alphabetically
+
+
+// 5.
+// Find the first string in arrayOfStrings with length of 3
+
+
+// 6.
+// Remove the last element from arrayOfNumbers
+
+
+// 7.
+// Remove the first element from arrayOfNumbers
+
